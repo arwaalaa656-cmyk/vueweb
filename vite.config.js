2a445255e019981d7564478c65e99302e71d45ef
arwaalaa656-cmyk <arwaalaa656@gmail.com>
2026-01-20 23:12:29 +0200

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,14 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
-
 // https://vite.dev/config/
 export default defineConfig({
+  base:'vueweb',
   plugins: [
+
     vue(),
     vueDevTools(),
         tailwindcss(),
-
   ],
   resolve: {
     alias: {
